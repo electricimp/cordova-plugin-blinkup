899a38ee24e84e18a31f032cc9c9ba2bf9141ad8
Paulo Vitor Magacho da Silva <pvmagacho@gmail.com>
2017-10-04 20:51:39 -0300

    Re-brand

--- www/blinkup.js
+++ www/blinkup.js
@@ -1,42 +1,40 @@
-/*
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- * http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- *
- * Created by Stuart Douglas (sdouglas@macadamian.com) on June 11, 2015.
- * Copyright (c) 2015 Macadamian. All rights reserved.
- */
 
-/*global cordova, module*/
 
-module.exports = {
-    /** startBlinkUp - starts the blinkup process
-    * @param {apiKey}: your blinkup api key
-    * @param {developerPlanId}: your development plan Id. Will be disregarded when {isInDevelopment} is set to false
-    * @param {isInDevelopment}: TRUE if you are connecting to development devices. when you are moving to production devices, this must be set to FALSE.
-    * @param {timeoutMS}: Amount of second before the application times out. Default & Maximum value is 60000.
-    */
-    startBlinkUp: function (apiKey, developerPlanId, isInDevelopment, timeoutMs, successCallback, errorCallback) {
-        cordova.exec(successCallback, errorCallback, "cordova-blinkup-plugin", "startBlinkUp", [apiKey, developerPlanId, isInDevelopment, timeoutMs]);
-    },
-    /**
-    * @deprecated Since version 1.1. Will be deleted in version 2.0. Use startBlinkUp instead.
-    */
-    invokeBlinkUp: function (apiKey, developerPlanId, timeoutMs, generateNewPlanId, successCallback, errorCallback) {
-        cordova.exec(successCallback, errorCallback, "cordova-blinkup-plugin", "invokeBlinkUp", [apiKey, developerPlanId, timeoutMs, generateNewPlanId]);
-    },
-    abortBlinkUp: function (successCallback, errorCallback) {
-        cordova.exec(successCallback, errorCallback, "cordova-blinkup-plugin", "abortBlinkUp", []);
-    },
-    clearBlinkUpData: function (successCallback, errorCallback) {
-        cordova.exec(successCallback, errorCallback, "cordova-blinkup-plugin", "clearBlinkUpData", []);
-    }
+var argscheck = require('cordova/argscheck'),
+    utils = require('cordova/utils'),
+    exec = require('cordova/exec');
+
+var Blinkup = function() {
 };
+
+/**
+ * startBlinkUp - starts the blinkup process
+ * @param {apiKey}: your blinkup api key
+ * @param {developerPlanId}: your development plan Id. Will be disregarded when {isInDevelopment} is set to false
+ * @param {isInDevelopment}: TRUE if you are connecting to development devices. when you are moving to production devices, this must be set to FALSE.
+ * @param {timeoutMS}: Amount of second before the application times out. Default & Maximum value is 60000.
+ */
+Blinkup.prototype.startBlinkUp = function (apiKey, developerPlanId, isInDevelopment, timeoutMs, successCallback, errorCallback) {
+  exec(successCallback, errorCallback, "cordova-plugin-blinkup", "startBlinkUp", [apiKey, developerPlanId, isInDevelopment, timeoutMs]);
+};
+
+/**
+ * invokeBlinkUp - invoke flash controller
+ * @param {apiKey}: your blinkup api key
+ * @param {developerPlanId}: your development plan Id. Will be disregarded when {isInDevelopment} is set to false
+ * @param {isInDevelopment}: TRUE if you are connecting to development devices. when you are moving to production devices, this must be set to FALSE.
+ * @param {timeoutMS}: Amount of second before the application times out. Default & Maximum value is 60000.
+ */
+Blinkup.prototype.invokeBlinkUp: function (apiKey, developerPlanId, timeoutMs, generateNewPlanId, successCallback, errorCallback) {
+  exec(successCallback, errorCallback, "cordova-plugin-blinkup", "invokeBlinkUp", [apiKey, developerPlanId, timeoutMs, generateNewPlanId]);
+};
+
+Blinkup.prototype.abortBlinkUp: function (successCallback, errorCallback) {
+  exec(successCallback, errorCallback, "cordova-plugin-blinkup", "abortBlinkUp", []);
+};
+
+Blinkup.prototype.clearBlinkUpData: function (successCallback, errorCallback) {
+  exec(successCallback, errorCallback, "cordova-plugin-blinkup", "clearBlinkUpData", []);
+};
+
+module.exports = Blinkup;
