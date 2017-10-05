fd7e165be78b7954b89ec12487c448b1856a6826
Paulo Vitor Magacho da Silva <pvmagacho@gmail.com>
2017-10-05 20:23:07 -0300

    Prepare for ionic usage

--- www/blinkup.js
+++ www/blinkup.js
@@ -5,17 +5,25 @@ var argscheck = require('cordova/argscheck'),
     exec = require('cordova/exec');
 
 var Blinkup = function() {
+  this.name = "Blinkup";
 };
 
 /**
  * startBlinkUp - starts the blinkup process
- * @param {apiKey}: your blinkup api key
- * @param {developerPlanId}: your development plan Id. Will be disregarded when {isInDevelopment} is set to false
- * @param {isInDevelopment}: TRUE if you are connecting to development devices. when you are moving to production devices, this must be set to FALSE.
- * @param {timeoutMS}: Amount of second before the application times out. Default & Maximum value is 60000.
+ * @param {module:blinkup.onSuccess} successCallback
+ * @param {module:blinkup.onError} errorCallback
+ * @param {module:blinkup.BlinkupOptions} options CameraOptions
  */
-Blinkup.prototype.startBlinkUp = function (apiKey, developerPlanId, isInDevelopment, timeoutMs, successCallback, errorCallback) {
-  exec(successCallback, errorCallback, "cordova-plugin-blinkup", "startBlinkUp", [apiKey, developerPlanId, isInDevelopment, timeoutMs]);
+Blinkup.prototype.startBlinkUp = function (successCallback, errorCallback, options) {
+  argscheck.checkArgs('fFO', 'Blinkup.startBlinkUp', arguments);
+  options = options || {};
+  var getValue = argscheck.getValue;
+
+  var apiKey = getValue(options.apiKey, "");
+  var developerPlanId = getValue(options.developerPlanId, "");
+  var isInDevelopment = !!options.isInDevelopment;
+  var timeoutMs = getValue(options.timeoutMs, 30000);
+  exec(successCallback, errorCallback, "Blinkup", "startBlinkUp", [apiKey, developerPlanId, isInDevelopment, timeoutMs]);
 };
 
 /**
@@ -26,15 +34,25 @@ Blinkup.prototype.startBlinkUp = function (apiKey, developerPlanId, isInDevelopm
  * @param {timeoutMS}: Amount of second before the application times out. Default & Maximum value is 60000.
  */
 Blinkup.prototype.invokeBlinkUp: function (apiKey, developerPlanId, timeoutMs, generateNewPlanId, successCallback, errorCallback) {
-  exec(successCallback, errorCallback, "cordova-plugin-blinkup", "invokeBlinkUp", [apiKey, developerPlanId, timeoutMs, generateNewPlanId]);
+  argscheck.checkArgs('fFO', 'Blinkup.invokeBlinkUp', arguments);
+  options = options || {};
+  var getValue = argscheck.getValue;
+
+  var apiKey = getValue(options.apiKey, "");
+  var developerPlanId = getValue(options.developerPlanId, "");
+  var isInDevelopment = !!options.isInDevelopment;
+  var timeoutMs = getValue(options.timeoutMs, 30000);
+  exec(successCallback, errorCallback, "Blinkup", "invokeBlinkUp", [apiKey, developerPlanId, isInDevelopment, timeoutMs]);
 };
 
 Blinkup.prototype.abortBlinkUp: function (successCallback, errorCallback) {
-  exec(successCallback, errorCallback, "cordova-plugin-blinkup", "abortBlinkUp", []);
+  argscheck.checkArgs('fF', 'Blinkup.abortBlinkUp', arguments);
+  exec(successCallback, errorCallback, "Blinkup", "abortBlinkUp", []);
 };
 
 Blinkup.prototype.clearBlinkUpData: function (successCallback, errorCallback) {
-  exec(successCallback, errorCallback, "cordova-plugin-blinkup", "clearBlinkUpData", []);
+  argscheck.checkArgs('fF', 'Blinkup.clearBlinkUpData', arguments);
+  exec(successCallback, errorCallback, "Blinkup", "clearBlinkUpData", []);
 };
 
 module.exports = Blinkup;
